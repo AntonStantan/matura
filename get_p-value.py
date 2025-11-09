7f9290c5159c953b2ace3b7fde0de91d03ab1379
Anton Mukin <mukinanton3@gmail.com>
2025-11-09 15:46:21 +0100

    soemthing is weird

--- get_p-value.py
+++ get_p-value.py
@@ -1,14 +1,13 @@
 from scipy.stats import ttest_1samp
 import math
 
-#values taken from the notebooks, where the p-value was calculated slightly inaccurately.
-benchmark_list = ['10.600075', '12.701344', '10.435802', '10.592991', '9.183982']
-[int(i) for i in benchmark_list]
+# Values taken from the notebooks, where the p-value was calculated slightly inaccurately.
+benchmark_list = [3.186406532665616, 3.0491308393076566, 7.784895952475832, 5.3125465253608235, 4.522614555350064]
+numeric_values = [float(i) for i in benchmark_list]
 
-log_transform = math.log(benchmark_list)
-
+log_transform = [math.log(x) for x in numeric_values]
 print(log_transform)
 
-stats, p_value = ttest_1samp(benchmark_list, popmean = 0)
+stats, p_value = ttest_1samp(log_transform, popmean = 0)
 
 print(f"p-value of the benchmarks: {p_value}")
